8b22332b7595c5291c2ef4379da275fe5e691be4
Nmerryman <nmerryman9001@gmail.com>
2023-06-01 04:19:54 -0500

    Added some documentation and cleaned up the code

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-My goal of this project is simply to help automate matching any input cve to any cwe. For now we won't do any tree traversal to find one that lands in view 699.
+# Goals
+My goal of this project is simply to help automate matching any input cve to any cwe. For now we won't do any tree traversal to try to find one that lands in view 699.
+
+# How to use
+## Compilation
+The program is in the `.nim` file which is compiled with the [nim programming langage](nim-lang.org). 
+I simply used the latest stable version.
+
+The command to compile is `nim c -d:ssl -d:release .\cve_rev.nim` and will produce `cve_rev.exe`.
+
+## Use
+- Run the executable in a command line.
+- Paste in the Cve full cve id such as `CVE-2007-2723`.
+- Choose search keywords
+- select closest matching Cwe
--- cve_rev.nim
+++ cve_rev.nim
@@ -1,5 +1,6 @@
 import std/[httpclient, strutils, json, sets, re, sequtils, strscans, os]
 
+# Types needed for collecting data
 type
     Cve = object
         id: string
@@ -16,25 +17,36 @@ type
         data: seq[Cve_to_Cwe]
     ChangeQuery = ref CatchableError
 
-
 proc get_cve_info(cve_val: string): Cve =
+    ## Get basic CVE info based on the id
+    
+    # Load api response
     let client = newHttpClient()
     let response = client.getContent("https://cveawg.mitre.org/api/cve/" & cve_val)
+    # debug
+    # writeFile("cve.html", response)
 
+    # Parse and generate Cve storage object
     let json_obj = parseJson(response)
     return Cve(id: cve_val, description: json_obj["containers"]["cna"]["descriptions"][0]["value"].getStr())
 
 proc extract_keywords(text: Cve): seq[seq[string]] =
+    ## Remove rarely important words and nouns that usually have nothing to do with the weakness itself
+    ## Organized so that runs of big words are kept together
+    
+    # Some (growing) criteria to check words against
     let blacklist_common = toHashSet(["a", "and", "as", "in", "to", "or", "of", "via", "used", "before",
     "after", "cause", "allows", "do", "when", "the", "has", "been", "which", "that", "from", "an"])
     let remove_chars = {'(', ')', '"', ',', '.'}
+
     var temp: seq[string]
     var i = 0
     var s_start = true
     var vals = text.description.split(" ").toSeq
+    # Double loop so we can nest sequences
     while i < vals.len:
         while i < vals.len:
-            # if first char is upper case (not start of sentence)
+            # if first char is upper case, (not start of sentence), it's probably a nown
             if vals[i][0].isUpperAscii and not s_start:
                 break
 
@@ -57,6 +69,7 @@ proc extract_keywords(text: Cve): seq[seq[string]] =
             if vals[i] in blacklist_common:
                 break
 
+            # Part of running sequence
             temp.add(vals[i])
             i += 1
 
@@ -64,27 +77,40 @@ proc extract_keywords(text: Cve): seq[seq[string]] =
         if temp.len > 0:
             result.add(temp)
             temp.setLen(0)
+    
+    return result
 
 proc gen_query(data: seq[seq[string]], query_opts: string): string =
-    ## Turn cl input into a human query
+    ## Turn input + keyword data into a human query
+    
     var parts = query_opts.split()
     for num_a, a in parts:
+        # Directly store anything following the 'c' as verbetim
         if a == "c":
             result &= parts[num_a + 1 .. parts.high].join(" ")
             break
         result &= data[parseInt(a)].join(" ") & " "
     
+    # Delete a trailing space if it exists
     if result[^1] == ' ':
         result.setLen(result.len - 1)
 
+    return result
+
 proc perform_search(query: string): seq[Cwe] =
+    ## Use the Google search engine on the CWE site to search for our key words
+
     # This base request simply needs to be updated when the token expires
+    # Maybe add a way to set this dynamically from the command line?
     var base_request = "https://cse.google.com/cse/element/v1?rsz=filtered_cse&num=10&hl=en&source=gcsc&gss=.com&cselibv=8e77c7877b8339e2&cx=012899561505164599335%3Atb0er0xsk_o&q=test_asdf&safe=off&cse_tok=AFW0emw-cDjP0r_IoYBy22b-oq-s%3A1685565480543&exp=csqr%2Ccc%2Cbf&oq=test_asdf&gs_l=partner-generic.3...856269.858334.4.858558.0.0.0.0.0.0.0.0..0.0.csems%2Cnrl%3D10...0....1.34.partner-generic..0.0.0.&callback=google.search.cse.api6724"
     var query_prepared = query.replace(' ', '+')
     var raw_request = base_request.replace("test_asdf", query_prepared)
     var client = newHttpClient()
     var response = client.getContent(raw_request)
+    # debug
+    # writeFile("google.html", response)
 
+    # Remove all js function calls until we only have a json object
     var start = 0
     var stop = 1
     while response[start] != '{':
@@ -93,41 +119,53 @@ proc perform_search(query: string): seq[Cwe] =
         stop += 1
     var parsed = parseJson(response[start .. ^stop])
 
+    # Iterate the results and store interestin Cwe info
     for a in parsed["results"]:
-        try:
-            var tid, ttitle: string
-            if scanf(a["title"].getStr(), "CWE-$+: $+", tid, ttitle):
-                var temp: Cwe
-                temp.name = ttitle
-                temp.id = tid
-                temp.description = a["contentNoFormatting"].getStr()
-                temp.link = a["unescapedUrl"].getStr()
-                result.add(temp)
-        except:
-            raise getCurrentException()
+        var tid, ttitle: string
+        if scanf(a["title"].getStr(), "CWE-$+: $+", tid, ttitle):
+            var temp: Cwe
+            ttitle.removeSuffix(" - CWE")
+            ttitle.removeSuffix(" (4.11)")
+            temp.name = ttitle
+            temp.id = tid
+            temp.description = a["contentNoFormatting"].getStr()
+            temp.link = a["unescapedUrl"].getStr()
+            result.add(temp)
+    
+    return result
 
 proc select_cwe(opts: seq[Cwe]): Cwe =
+    ## Display the sequence of Cwe objects and let the user select one of them
+
     echo "Select one of the following, c to change query"
     for i in 0 .. opts.high:
         echo i, ": ", opts[i].name
+        echo "\t", opts[i].description
     var input = stdin.readLine()
+    # By using an exception here we can catch easily catch it outside of this function
     if input == "c":
         raise ChangeQuery()
     return opts[input.parseInt()]
 
 proc update_data(cve: Cve, cwe: Cwe) =
+    ## Store the decision in a local file but try to be context aware
+
+    # Create file if it doesn't already exist
     var data: Data_collection
     if fileExists("mapping.json"): 
        data = to(parseFile("mapping.json"), Data_collection)
     
+    # Add run to collection
     data.data.add(Cve_to_Cwe(cve: cve, cwe: cwe))
     var temp_out: string
+
+    # Store the data again
     # Use toUgly in the future for speed
     temp_out = pretty(%*(data))
     writeFile("mapping.json", temp_out )
 
 proc test =
-    # Test the system with no user input
+    ## Test the system with no user input
     var cve = "CVE-2010-3257"
     var desc = get_cve_info(cve)
     echo desc
@@ -147,27 +185,32 @@ proc test =
     
 
 proc main() =
+    # Another loop so I can do multiple iterations
     while true:
-        try:
-            echo "What is the CVE number"
-            var cid_raw = stdin.readLine()
-            var cve = get_cve_info(cid_raw)
-            var parts = extract_keywords(cve)
-            for i in 0..parts.high:
-                echo i, ": ", parts[i].join(" ")
-            echo "Number to craft query, c for custom"
-            var query_opts = stdin.readLine()
-            var query_prep = gen_query(parts, query_opts)
-            echo "Trying: ", query_prep
-            var search_res = perform_search(query_prep)
-            var chosen = select_cwe(search_res)
-            update_data(cve, chosen)
-            echo "Saved Cve -> Cwe mapping"
-            break
-        except ChangeQuery:
-            continue
+        echo "What is the CVE number"
+        var cid_raw = stdin.readLine()
+        while true:
+            try:
+                var cve = get_cve_info(cid_raw)
+                var parts = extract_keywords(cve)
+                for i in 0..parts.high:
+                    echo i, ": ", parts[i].join(" ")
+                echo "Number to craft query (space delimited for multiple), c for custom, f to print full description"
+                var query_opts = stdin.readLine()
+                if query_opts == "f":
+                    echo cve.description
+                    continue
+                var query_prep = gen_query(parts, query_opts)
+                echo "Trying: ", query_prep
+                var search_res = perform_search(query_prep)
+                var chosen = select_cwe(search_res)
+                update_data(cve, chosen)
+                echo "Saved Cve -> Cwe mapping"
+                break
+            except ChangeQuery:
+                continue
 
 
 if isMainModule:
-    # main()
-    test()
+    main()
+    # test()
